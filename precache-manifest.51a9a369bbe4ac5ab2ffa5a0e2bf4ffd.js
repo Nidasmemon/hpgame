92c06a37a861fe9817d8592cf2eefb83967d09c0
Nida Memon <nidasmemon@gmail.com>
2020-05-04 22:20:06 -0700

    Updates

---
+++ precache-manifest.51a9a369bbe4ac5ab2ffa5a0e2bf4ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bb79b6edb9449afd809a0e09a6a096",
+    "url": "/hpgame/index.html"
+  },
+  {
+    "revision": "737ed1939f91486a641d",
+    "url": "/hpgame/static/css/main.cdc35d3c.chunk.css"
+  },
+  {
+    "revision": "f60ce54cea99903a3aa3",
+    "url": "/hpgame/static/js/2.a871daff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hpgame/static/js/2.a871daff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737ed1939f91486a641d",
+    "url": "/hpgame/static/js/main.4b92ddae.chunk.js"
+  },
+  {
+    "revision": "8762296d100d01b269c9",
+    "url": "/hpgame/static/js/runtime-main.66ff9dad.js"
+  }
+]);
